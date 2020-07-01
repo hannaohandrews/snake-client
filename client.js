5b2c1b0887d970c593eb47c9b6de1262c7c390b1
hannaohandrews <hannaohandrews@gmail.com>
2020-07-01 21:45:28 +0000

    third edit

--- client.js
+++ client.js
@@ -7,16 +7,28 @@ const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
-
   });
-  
+
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+    //sending message to ther server
+    conn.write('Name: HOA');
+    //conn.write('Move: up');
+  })
+
+
   conn.on('data', (data) => {
-    console.log('you ded cuz you idled >>', data);
+    console.log(data);
   });
   return conn;
+
+  
+
 };
+
 
 module.exports = {connect};
 
