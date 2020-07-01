790f6851c278b8e75b50f73ce48cabea7105ae74
hannaohandrews <hannaohandrews@gmail.com>
2020-07-01 21:19:40 +0000

    2nd

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require('net');
-/**
- * Establishes connection with the game server
- */
-const connect = function () {
-
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log('you ded cuz you idled >>', data);
-  });
-  return conn;
-;}
+// play.js
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
