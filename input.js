d85c2f8f48c2b20c406cb9ba64ca7f172950aff8
hannaohandrews <hannaohandrews@gmail.com>
2020-07-01 22:51:25 +0000

    done

--- input.js
+++ input.js
@@ -1,4 +1,10 @@
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  
+  connection = conn;
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,6 +16,18 @@ const setupInput = function() {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 }
 
